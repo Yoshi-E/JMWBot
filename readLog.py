9ec68d4c47445c93a94a4af301a8d9ab328bbd16
Yoshi-E <server19960714@yahoo.de>
2018-10-22 12:38:02 +0200

    test

--- readLog.py
+++ readLog.py
@@ -24,19 +24,13 @@ def readData(admin, gameindex):
     name = logs[logindex] #fetch last log file
     collected_rows = scanfile(name)
     #if data is also in previous logs, search there, until 2 game ends are found
-    print(logindex*-1) #1
-    print(gameindex) #1
-    print(len(collected_rows)) #1
-    print(len(logs)) #175
-    print("-----")
-    while((logindex*-1) < 10 and (logindex*-1) < len(logs) and gameindex <= len(collected_rows)): 
+    while((logindex*-1) < 10 and (logindex*-1) < len(logs) and gameindex >= len(collected_rows)): 
         logindex = logindex -1
         name = getLogs()[logindex] #fetch previous log file
         print("next scan: "+name)
         p = scanfile(name)[:-1]
         collected_rows[0][0] = (p[-1][0]) + (collected_rows[0][0])#combine data from previous 
         collected_rows = collected_rows + p[:-1]
-        print("len:"+len(collected_rows))
     
     for row in collected_rows:
         print(row[2])
