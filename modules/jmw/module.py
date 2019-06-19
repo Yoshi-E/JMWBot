ff3f6a374f7d56889c9ed8cc3dde1d95f53dfac0
Yoshi-E <server19960714@yahoo.de>
2019-06-19 23:43:59 +0200

    fixes

--- modules/jmw/module.py
+++ modules/jmw/module.py
@@ -51,7 +51,7 @@ class CommandJMW(commands.Cog):
         for user in self.user_data:
             if "nextgame" in self.user_data[user] and self.user_data[user]["nextgame"] == True:
                 print("sending DM to: "+str(user))
-                puser = await self.bot.get_user(int(user))
+                puser = self.bot.get_user(int(user))
                 await puser.send(msg)  
                 self.user_data[user]["nextgame"] = False
         await self.set_user_data() #save changes
@@ -234,7 +234,7 @@ class CommandJMW(commands.Cog):
             #store data, to remind user later on
             await self.set_user_data(str(tauthor), "nextgame" , True)
             msg = ':white_check_mark: Ok, I will send you a message when you can join for a new round.'
-        puser = await self.bot.get_user(tauthor)
+        puser = self.bot.get_user(tauthor)
         await puser.send(msg)  
 
         
@@ -300,7 +300,7 @@ class CommandJMW(commands.Cog):
                 await coro()
             except Exception as ex:
                 ex = str(ex)+"/n"+str(traceback.format_exc())
-                user=await self.bot.get_user(165810842972061697)
+                user=self.bot.get_user(165810842972061697)
                 await user.send("Caught exception")
                 await user.send(ex[:1800] + '..' if len(ex) > 1800 else ex)
                 logging.error('Caught exception')
